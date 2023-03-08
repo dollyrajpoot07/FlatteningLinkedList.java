0140364aa6822471a1c19c9a4ea6733cf3ffc0da
Dolly <dollyrajpoot07@gmail.com>
2023-03-08 22:46:59 +0530

    Initial commit

---
+++ FlatteningLinkedList.java
@@ -0,0 +1,105 @@
+// Given a linked list where every node represents a linked list and contains two pointers of its type: 
+
+// Pointer to next node in the main list (we call it ‘right’ pointer in the code below) 
+// Pointer to a linked list where this node is headed (we call it the ‘down’ pointer in the code below). 
+
+// Input:    5 -> 10 -> 19 -> 28
+//                |        |         |        |
+//               V       V       V       V
+//               7      20      22       35
+//               |              |        |
+//               V               V       V
+//               8               50     40
+//               |                       |
+//              V                        V
+
+
+// Output: 5->7->8->10->19->20->22->28->30->35->40->45->50
+
+
+class LinkedList {
+	Node head;
+	class Node {
+		int data;
+		Node right, down;
+		Node(int data) {
+			this.data = data;
+			right = null;
+			down = null;
+		}
+	}
+	Node merge(Node a, Node b) {
+
+		if (a == null)
+			return b;
+
+		if (b == null)
+			return a;
+
+		Node result;
+
+		if (a.data < b.data) {
+			result = a;
+			result.down = merge(a.down, b);
+		}
+
+		else {
+			result = b;
+			result.down = merge(a, b.down);
+		}
+
+		result.right = null;
+		return result;
+	}
+
+	Node flatten(Node root) {
+		if (root == null || root.right == null)
+			return root;
+
+		root.right = flatten(root.right);
+
+		root = merge(root, root.right);
+
+		return root;
+	}
+
+	Node push(Node head_ref, int data)	{
+		Node new_node = new Node(data);
+		new_node.down = head_ref;
+		head_ref = new_node;
+		return head_ref;
+	}
+
+	void printList() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + " ");
+			temp = temp.down;
+		}
+		System.out.println();
+	}
+
+	public static void main(String args[])	{
+		LinkedList L = new LinkedList();
+		L.head = L.push(L.head, 30);
+		L.head = L.push(L.head, 8);
+		L.head = L.push(L.head, 7);
+		L.head = L.push(L.head, 5);
+
+		L.head.right = L.push(L.head.right, 20);
+		L.head.right = L.push(L.head.right, 10);
+
+		L.head.right.right = L.push(L.head.right.right, 50);
+		L.head.right.right = L.push(L.head.right.right, 22);
+		L.head.right.right = L.push(L.head.right.right, 19);
+
+		L.head.right.right.right = L.push(L.head.right.right.right, 45);
+		L.head.right.right.right = L.push(L.head.right.right.right, 40);
+		L.head.right.right.right = L.push(L.head.right.right.right, 35);
+		L.head.right.right.right = L.push(L.head.right.right.right, 20);
+
+		L.head = L.flatten(L.head);
+
+		L.printList();
+	}
+}
